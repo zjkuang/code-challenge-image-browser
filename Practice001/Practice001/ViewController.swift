ddbc100c7b48f8ca0db40466aa367a54dcf8f403
John Kuang <john.kuang@robotsandpencils.com>
2020-06-13 09:44:27 -0600

    An empty, storyboardless single view app

---
+++ Practice001/Practice001/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Practice001
+//
+//  Created by Zhengqian Kuang on 2020-06-13.
+//  Copyright © 2020 Kuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
